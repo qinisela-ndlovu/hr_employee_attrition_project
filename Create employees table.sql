bc4e268a82400f4e940a78121a1844377bd519fb
qinisela-ndlovu <qqinisela982@gmail.com>
2025-06-03 12:04:39 +0200

    Delete Create employees table.sql

--- Create employees table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE hrdata.employees (
-    Age INT, Attrition VARCHAR(3), BusinessTravel VARCHAR(50),DailyRate INT, Department VARCHAR(50), DistanceFromHome INT, Education INT, EducationField VARCHAR(100), EmployeeCount INT, EmployeeNumber INT PRIMARY KEY,
-    EnvironmentSatisfaction INT,Gender VARCHAR(10),HourlyRate INT,JobInvolvement INT,JobLevel INT,JobRole VARCHAR(100),JobSatisfaction INT,MaritalStatus VARCHAR(20),MonthlyIncome INT,
-    MonthlyRate INT,NumCompaniesWorked INT,Over18 VARCHAR(3),OverTime VARCHAR(5),PercentSalaryHike INT,PerformanceRating INT,RelationshipSatisfaction INT,StandardHours INT,StockOptionLevel INT,
-    TotalWorkingYears INT,TrainingTimesLastYear INT,WorkLifeBalance INT,YearsAtCompany INT,YearsInCurrentRole INT,YearsSinceLastPromotion INT,YearsWithCurrManager INT,
-    Attrition_Value INT
-);
